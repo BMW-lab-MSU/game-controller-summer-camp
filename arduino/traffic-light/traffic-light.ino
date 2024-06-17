6d3c256164d134359f06b9a792a3f829cca36ed5
Trevor Vannoy <trevor@vannoy.fyi>
2024-06-17 16:21:29 -0600

    add comments to traffic light example

--- arduino/traffic-light/traffic-light.ino
+++ arduino/traffic-light/traffic-light.ino
@@ -1,5 +1,5 @@
 void setup() {
-  // put your setup code here, to run once:
+  // set the LEDs as outputs
   pinMode(A2, OUTPUT);  // green
   pinMode(A5, OUTPUT);  // yellow
   pinMode(A9, OUTPUT);  // red
@@ -7,15 +7,20 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(A2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // Turn on the green light for 1 second
+  // Make sure all the other lights are off.
+  digitalWrite(A2, HIGH);
   digitalWrite(A5, LOW);
   digitalWrite(A9, LOW);
-  delay(1000);                       // wait for a second
-  digitalWrite(A2, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
+
+  // Turn on the yellow light for 0.5 seconds
+  digitalWrite(A2, LOW);
   digitalWrite(A5, HIGH);
   digitalWrite(A9, LOW);
   delay(500);
+
+  // Turn on the red light for 1 second	
   digitalWrite(A2, LOW);
   digitalWrite(A5, LOW);
   digitalWrite(A9, HIGH);
