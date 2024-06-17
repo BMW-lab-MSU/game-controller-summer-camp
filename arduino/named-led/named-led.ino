a816bd0071e2db4b31a9dec53cd402bb02d5e084
Trevor Vannoy <trevor@vannoy.fyi>
2024-06-17 16:44:14 -0600

    add example of naming an input/output pin with a variable

---
+++ arduino/named-led/named-led.ino
@@ -0,0 +1,16 @@
+// To make the code easier to read, we can name the pin with a more descriptive
+// name, such as blue_led if we are using a blue LED. This helps us keep track of
+// what each input or output pin is used for.
+int blue_led = A0;
+
+void setup() {
+  // Wire up the LED's anode (long leg, positive) to pin A0.
+  // Pin A0 needs to be an output so we can turn the LED on.
+  pinMode(blue_led, OUTPUT);
+
+}
+
+void loop() {
+  // Turn on the LED.
+  digitalWrite(blue_led, HIGH);
+}
